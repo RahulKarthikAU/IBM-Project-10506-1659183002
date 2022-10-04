5a197075b4e762e1620a92d8b0246ce46b29b079
tamilselvan3302 <tamilselvan0332@gmail.com>
2022-10-04 20:32:15 +0530

    Update with db

--- development_phase/backend/flaskr/__init__.py
+++ development_phase/backend/flaskr/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+    
+    from .utils.db import run_sql
+    run_sql('select * from user where id = ? and email = ?', (1, 'hello@124',))
 
     @app.route('/hello')
     def hello():
